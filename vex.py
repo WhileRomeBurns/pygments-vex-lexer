c921a01e2bff9a7e10632d3ac658315d969fa173
WhileRomeBurns <slipowski@gmail.com>
2025-11-15 23:55:17 -0500

    tons of fixes, better float and int support. added constants

--- vex.py
+++ vex.py
@@ -1,25 +1,19 @@
 """
     Vex Lexer
 """
-
 import os
 import re
 
-from pygments.lexer import RegexLexer, include, bygroups, inherit, words, \
-    default
+from pygments.lexer import RegexLexer, bygroups, inherit, words, default
 from pygments.token import Text, Comment, Operator, Keyword, Name, String, \
     Number, Punctuation, Whitespace
-
-#from pygments.lexers.c_cpp import CLexer, CppLexer
-#from pygments.lexers import _mql_builtins
 
 try:
     __file__
 except NameError:
     __file__ = 'vex.py'
 
-_ROOT = os.path.abspath(os.path.dirname(__file__))
-_ROOT = os.path.join(_ROOT, 'syntax')
+_ROOT = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'syntax')
 
 with open(os.path.join(_ROOT, "VexKeywords.txt")) as f:
     VEX_KEYWORDS = f.read().split()
@@ -29,11 +23,12 @@ with open(os.path.join(_ROOT, "VexTypes.txt")) as f:
     VEX_TYPES = f.read().split()
 with open(os.path.join(_ROOT, "VexPredefined.txt")) as f:
     VEX_PREDEFINED = f.read().split()
-with open(os.path.join(_ROOT, "VexPreprocessor.txt")) as f:
-    VEX_PREPROCESSOR = f.read().split()
+with open(os.path.join(_ROOT, "VexConstants.txt")) as f:
+    VEX_CONSTANTS = f.read().split()
+with open(os.path.join(_ROOT, "VexContexts.txt")) as f:
+    VEX_CONTEXTS = f.read().split()
 
 class VexLexer(RegexLexer):
-#class VexLexer(CLexer):
     """
     For Side Effects Vex source.
     """
@@ -46,8 +41,10 @@ class VexLexer(RegexLexer):
 
     vex_keywords = (words(VEX_KEYWORDS, suffix=r'\b'), Keyword.Reserved)
     vex_types = (words(VEX_TYPES, suffix=r'\b'), Keyword.Type)
-    vex_functions = (words(VEX_FUNCTIONS, suffix=r'\b'), Keyword.Type)
-    # vex_preprocessor = (words(VEX_PREPROCESSOR, prefix=r'^#', suffix=r'.*?\n'), Comment.Preproc)
+    vex_functions = (words(VEX_FUNCTIONS, suffix=r'\b(\s)*?(\()'), bygroups(Name.Function, Whitespace, Punctuation))
+    vex_predefined = (words(VEX_PREDEFINED, suffix=r'\b'), Name.Builtin)
+    vex_constants = (words(VEX_CONSTANTS, suffix=r'\b'), Name.Constant)
+    vex_contexts = (words(VEX_CONTEXTS, suffix=r'\b'), Keyword.Namespace)
 
     tokens = {
         'root': [
@@ -58,26 +55,31 @@ class VexLexer(RegexLexer):
             (r'//.*?\n', Comment.Single),
             (r'/\*', Comment.Multiline, 'comment-multiline'),
 
-            # Vex wrangles can inject expressions in backtick strings, macro-like
-            (r'`', String, 'string-backtick'), # should be first, h-expression eval early
+            # Backtick Expression Strings
+            (r'`', String, 'string-backtick'),
             
             # Keywords
             vex_keywords,
             vex_types,
+            #vex_functions,
+            vex_predefined,
+            vex_constants,
+            vex_contexts,
 
-            # Floats [todo, test 0.000_000_01]
-            (r'[0-9]+\.[0-9]+([eE][-+]?[0-9]+)?', Number.Float),
-            #(r'[0-9]+\.(?:[0-9][0-9][0-9]_)+[0-9]+', Number.Float),
-            #(r'[0-9]+\.?[eE][-+]?[0-9]+', Number.Float),
-
-            # Integers [todo, test 1_000_000]
+            # Floats
+            (r'[0-9]+\.(?:[0-9][0-9][0-9]_)+[0-9]+', Number.Float),
+            (r'[0-9]+\.(?:[eE][-+]?[0-9]+)?', Number.Float),
+            (r'[0-9]+\.[0-9]+(?:[eE][-+]?[0-9]+)?', Number.Float),
+            
+            # Integers
             (r'0b[01]+', Number.Bin),
             (r'0x[0-9a-fA-F]+', Number.Hex),
-            (r'[0-9]+', Number.Integer),
-            #(r'[0-9]+(?:_[0-9][0-9][0-9])+', Number.Integer),
+            (r'[0-9]+(?:_[0-9][0-9][0-9])+', Number.Integer),
+            (r'[0-9]+(?:[eE][-+]?[0-9]+)?', Number.Integer),
 
-            # Identifier
-            (r'[fuvpi234sd]?@[a-zA-Z_]\w*', Name.Builtin),
+            # Identifier, match bindings like v[]@ and v@ first and then known types @
+            (r'[fuvpi234sd]+(?:\[\])?@[a-zA-Z_]\w*', Name.Builtin),
+            (r'@[a-zA-Z_]\w*', Name.Builtin),
             (r'[a-zA-Z_]\w*', Name),
             
             # Strings
@@ -110,15 +112,12 @@ class VexLexer(RegexLexer):
 
     def get_tokens_unprocessed(self, text, stack=('root',)):
 
-        #for index, token, value in CLexer.get_tokens_unprocessed(self, text, stack):
         for index, token, value in RegexLexer.get_tokens_unprocessed(self, text, stack):
-            # if token is String:
-            #     print(value)
             if token is Name:
-                print(value)
                 if value in VEX_FUNCTIONS:
-                    token = Name.Function
+                    start = index + len(value)
+                    if start < len(text):
+                        match = re.match(r'\s*?\(', text[start:])
+                        if match:
+                            token = Name.Function
             yield index, token, value
-
-#if __name__ == '__main__':
-#    print(VEX_KEYWORDS)
